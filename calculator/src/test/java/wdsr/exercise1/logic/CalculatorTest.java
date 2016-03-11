43cf43a51093584c219873732487267fbeb7bdf6
Jakub Kulik <krulicz@interia.pl>
2016-03-11 19:16:41 +0100

    Added exercise1d

--- calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
+++ calculator/src/test/java/wdsr/exercise1/logic/CalculatorTest.java
@@ -1,6 +1,7 @@
 package wdsr.exercise1.logic;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -56,6 +57,56 @@ public class CalculatorTest {
 		
 		// when
 		calculator.min(values);
+		
+		// then
+		// empty
+	}	
+	
+	@Test
+	public void testMax_shouldReturnTheOnlyValue() {
+		// given
+		int[] values = { 55 };
+		
+		// when
+		int max = calculator.max(values);
+		
+		// then
+		//assertEquals(values[0], max);
+		assertThat("Its not the max value", values[0], is(max));
+	}
+	
+	@Test
+	public void testMax_shouldReturnTheHighestOfAllValues() {
+		// given
+		int[] values = { 2, -3, 5 };
+		
+		// when
+		int max = calculator.max(values);
+		
+		// then
+		//assertEquals(values[2], max);
+		assertThat("Its not the max value", values[2], is(max));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testMax_shouldRaiseAnExceptionForNullArgument() {
+		// given
+		int[] values = null;
+		
+		// when
+		calculator.max(values);
+		
+		// then
+		// empty
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testMax_shouldRaiseAnExceptionForEmptyArgument() {
+		// given
+		int[] values = {};
+		
+		// when
+		calculator.max(values);
 		
 		// then
 		// empty
